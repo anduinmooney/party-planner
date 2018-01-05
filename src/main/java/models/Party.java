aedc70b0cb77e1d7735b0b14cae02f318f300aa9
Anduin Mooney <anduinmooney@gmail.com>
2018-01-05 15:24:27 -0800

    Change strings to integers in Party.java to adjust price based on amount of food, beverages, entertainment instead of doing them each individually. Change test to cover guests, food, beverages, and entertainment. Fail test on overall price for party.

--- src/main/java/models/Party.java
+++ src/main/java/models/Party.java
@@ -2,31 +2,32 @@ package models;
 
 public class Party {
    private Integer guests;
-   private String food;
-   private String beverages;
-   private String entertainment;
+   private Integer food;
+   private Integer beverages;
+   private Integer entertainment;
    private int price;
-   public Party(Integer guests, String food, String beverages, String entertainment) { //include food, beverages, entertainment for future use
+   public Party(Integer guests, Integer food, Integer beverages, Integer entertainment) {
       this.guests = guests;
       this.food = food;
       this.beverages = beverages;
       this.entertainment = entertainment;
       this.price = this.guests * 5;
+      this.price = this.food * 50;
     }
     public int getGuests() {
       return this.guests;
     }
-    public String getFood() {
+    public int getFood() {
       return this.food;
     }
-    public String getDrink() {
+    public int getDrink() {
       return this.beverages;
     }
-    public String getEntertainment() {
+    public int getEntertainment() {
       return this.entertainment;
     }
     public int getPrice() {
-      return this.price;
+      return 0;
     }
 
 }
--- src/test/java/models/PartyTest.java
+++ src/test/java/models/PartyTest.java
@@ -9,32 +9,33 @@ import java.util.ArrayList;
 public class PartyTest {
     @Test
     public void newParty_partyCreation() {
-        Party testParty = new Party(50, "food", "beverages", "entertainment");
+        Party testParty = new Party(50, 3, 3, 3);
         assertEquals(true, testParty instanceof Party);
     }
     @Test
     public void newParty_getGuests_returnGuest() {
-        Party testParty = new Party(10, "food", "beverages", "entertainment");
+        Party testParty = new Party(10, 3, 3, 3);
         assertEquals(10, testParty.getGuests());
     }
     @Test
     public void newParty_getFood_returnFood() {
-        Party testParty = new Party(10, "food", "beverages", "entertainment");
-        assertEquals("food", testParty.getFood());
+        Party testParty = new Party(10, 3, 3, 3);
+        assertEquals(3, testParty.getFood());
     }
     @Test
     public void newParty_getDrink_returnDrink() {
-        Party testParty = new Party(10, "food", "beverages", "entertainment");
-        assertEquals("beverages", testParty.getDrink());
+        Party testParty = new Party(10, 3, 3, 3);
+        assertEquals(3, testParty.getDrink());
     }
     @Test
     public void newParty_getEntertainment_returnEntertainment() {
-        Party testParty = new Party(10, "food", "beverages", "entertainment");
-        assertEquals("entertainment", testParty.getEntertainment());
+        Party testParty = new Party(10, 3, 3, 3);
+        assertEquals(3, testParty.getEntertainment());
     }
     @Test
-    public void newParty_getPriceGuest_returnPriceGuest() {
-        Party testParty = new Party(10, "food", "beverages", "entertainment");
-        assertEquals(50, testParty.getPrice());
+    public void newParty_getPrice_returnPrice() {
+        Party testParty = new Party(10, 3, 3, 3);
+        assertEquals(150, testParty.getPrice());
     }
+
 }
