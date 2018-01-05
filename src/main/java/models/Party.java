dd2cf54fa7e34ef34cc05d864fd23fa66be0dd37
Anduin Mooney <anduinmooney@gmail.com>
2018-01-05 14:29:14 -0800

    Reinsert first test.

--- src/main/java/models/Party.java
+++ src/main/java/models/Party.java
@@ -2,7 +2,7 @@ package models;
 
 public class Party {
    private Integer guests;
-   public Party(Integer guests) {
+   public Party(Integer guests, String food, String beverages, String entertainment) {
       this.guests = guests;
     }
     public int getGuests() {
--- src/test/java/models/PartyTest.java
+++ src/test/java/models/PartyTest.java
@@ -7,14 +7,14 @@ import java.util.ArrayList;
 
 
 public class PartyTest {
-//    @Test
-//    public void newParty_partyCreation() {
-//        Party testParty = new Party(50, "food", "beverages", "entertainment");
-//        assertEquals(true, testParty instanceof Party);
-//    }
+    @Test
+    public void newParty_partyCreation() {
+        Party testParty = new Party(50, "food", "beverages", "entertainment");
+        assertEquals(true, testParty instanceof Party);
+    }
     @Test
     public void newParty_getGuests_returnTen() {
-        Party testParty = new Party(10);
+        Party testParty = new Party(10, "food", "beverages", "entertainment");
         assertEquals(10, testParty.getGuests());
     }
 }
