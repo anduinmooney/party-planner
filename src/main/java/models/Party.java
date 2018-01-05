761300fcee174d86449eb65666c13a104492b343
Anduin Mooney <anduinmooney@gmail.com>
2018-01-05 14:48:16 -0800

    Change drink to beverages for consistancy, pass test for beverages.

--- src/main/java/models/Party.java
+++ src/main/java/models/Party.java
@@ -3,11 +3,11 @@ package models;
 public class Party {
    private Integer guests;
    private String food;
-   private String drink;
+   private String beverages;
    public Party(Integer guests, String food, String beverages, String entertainment) { //include food, beverages, entertainment for future use
       this.guests = guests;
       this.food = food;
-      this.drink = drink;
+      this.beverages = beverages;
     }
     public int getGuests() {
       return this.guests;
@@ -16,7 +16,7 @@ public class Party {
       return this.food;
     }
     public String getDrink() {
-      return null;
+      return this.beverages;
     }
 
 }
